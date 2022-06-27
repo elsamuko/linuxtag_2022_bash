a79fd3b6c57dfcfe99b0dde4824a6bf5fef0c1bd
Samuel Albrecht <elsamuko@gmail.com>
2022-06-27 19:32:34 +0200

    add: more basics

--- basics.sh
+++ basics.sh
@@ -8,33 +8,56 @@ echo -e "\033[1;32mThis is green\033[0m"
 printf "%s\n" "Hallo bash"
 echo
 
+# functions
+function a_func {
+    echo "a_func was called"
+}
+
+# controls
+for i in 1 2 3; do
+    echo "loop $i"
+done
+
+COUNT=4
+while [ $COUNT != 0 ]; do
+    ((COUNT=COUNT-1)) # math
+    echo "COUNT : $COUNT"
+done
+
+if [ $COUNT -lt 2 ]; then
+    echo "COUNT is lower than 2"
+fi
+
+case "$1" in
+    blah)
+        echo "Called with blah"
+        ;;
+    blubb)
+        echo "Called with blubb"
+        ;;
+    d*)
+        echo "Called with d*"
+        ;;
+    *)
+        echo "No matching argument"
+        ;;
+esac
+echo
+
 # variables
 HASE=1
-echo $HASE
+echo "HASE : $HASE"
+function func { local FOO="foo"; echo "$FOO"; } # only within functions
+export BAR="bar" # available within subshells
+: "${BLUBB:=blubb}" # default assignment
+echo "BLUBB=$BLUBB"
 
-# files
-echo "creating a file"
-touch a_file
-ls -l a_file
-echo "removing this file"
-rm a_file
-ls -l a_file
+# arguments
+FIRST="${1:-first}" # default argument
+echo "FIRST : $FIRST"
+echo "This script was called with $# arguments"
+for ARG in "$@"; do
+    echo "$ARG"
+done
 echo
-
-echo "write" > a_text_file # overwrite with >
-echo "something" >> a_text_file # append with >>
-
-# read whole file
-CONTENT=$(<a_text_file)
-echo "whole : $CONTENT"
-echo
-
-# read line by line
-while read -r LINE; do
-    echo "line : $LINE"
-done < a_text_file
-echo
-
-# variables
-
 
